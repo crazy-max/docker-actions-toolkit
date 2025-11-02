1ac004634a3182978fe206c23599ad5382a38ccd
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-11-02 02:20:51 +0100

    push lib for testing
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

---
+++ lib/sigstore/sigstore.d.ts
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2025 actions-toolkit authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Attestation } from '@actions/attest';
+import { Cosign } from '../cosign/cosign';
+import { ImageTools } from '../buildx/imagetools';
+import { Subject } from '../types/intoto/intoto';
+export interface SignAttestationManifestsOpts {
+    imageName: string;
+    imageDigest: string;
+    noTransparencyLog?: boolean;
+}
+export interface SignAttestationManifestsResult extends Attestation {
+    imageName: string;
+}
+export interface VerifySignedManifestsOpts {
+    certificateIdentityRegexp: string;
+    retries?: number;
+}
+export interface VerifySignedManifestsResult {
+    cosignArgs: Array<string>;
+    signatureManifestDigest: string;
+}
+export interface SignProvenanceBlobsOpts {
+    localExportDir: string;
+    name?: string;
+    noTransparencyLog?: boolean;
+}
+export interface SignProvenanceBlobsResult extends Attestation {
+    bundlePath: string;
+    subjects: Array<Subject>;
+}
+export interface VerifySignedArtifactsOpts {
+    certificateIdentityRegexp: string;
+}
+export interface VerifySignedArtifactsResult {
+    bundlePath: string;
+    cosignArgs: Array<string>;
+}
+export interface SigstoreOpts {
+    cosign?: Cosign;
+    imageTools?: ImageTools;
+}
+export declare class Sigstore {
+    private readonly cosign;
+    private readonly imageTools;
+    constructor(opts?: SigstoreOpts);
+    signAttestationManifests(opts: SignAttestationManifestsOpts): Promise<Record<string, SignAttestationManifestsResult>>;
+    verifySignedManifests(opts: VerifySignedManifestsOpts, signed: Record<string, SignAttestationManifestsResult>): Promise<Record<string, VerifySignedManifestsResult>>;
+    signProvenanceBlobs(opts: SignProvenanceBlobsOpts): Promise<Record<string, SignProvenanceBlobsResult>>;
+    verifySignedArtifacts(opts: VerifySignedArtifactsOpts, signed: Record<string, SignProvenanceBlobsResult>): Promise<Record<string, VerifySignedArtifactsResult>>;
+    private signingEndpoints;
+    private static noTransparencyLog;
+    private static getProvenanceBlobs;
+    private static getProvenanceSubjects;
+    private static toAttestation;
+}
